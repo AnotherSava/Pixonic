9b486240d13b8bbbffec030754a8e9bcc28a00f9
Oleg Savelyev <Oleg.Savelev@gmail.com>
2017-04-09 16:12:18 +0300

    migrated to maven

---
+++ src/test/java/SleepingTestTaskRecord.java
@@ -0,0 +1,45 @@
+import java.time.LocalDateTime;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import static java.lang.Thread.sleep;
+
+public class SleepingTestTaskRecord extends TaskRecord
+{
+	private int id;
+
+	/**
+	 * Тестовое задание: ждёт некоторое время, потом добавляет свой идентификатор в список (выполненного)
+	 * @param time время начала
+	 * @param workMS время эмулирования деятельности (мс)
+	 * @param taskLog список, где отметиться по завершении
+	 * @param id идентификатор (предположительно, уникальный)
+	 */
+	public SleepingTestTaskRecord(LocalDateTime time, long workMS, CopyOnWriteArrayList<Integer> taskLog, int id)
+	{
+		super(time, (() ->
+		{
+			if (workMS > 0)
+				sleep(workMS);
+			taskLog.add(id);
+			return null;
+		}));
+		this.id = id;
+	}
+
+	/**
+	 * Тестовое задание: ждёт некоторое время, потом добавляет свой идентификатор в список (выполненного)
+	 * @param delayMS время начала работы относительно текущего момента (мс)
+	 * @param workMS время эмулирования деятельности (мс)
+	 * @param taskLog список, где отметиться по завершении
+	 * @param id идентификатор (предположительно, уникальный)
+	 */
+	public SleepingTestTaskRecord(long delayMS, long workMS, CopyOnWriteArrayList<Integer> taskLog, int id)
+	{
+		this(LocalDateTime.now().plusNanos(delayMS * 1000000), workMS, taskLog, id);
+	}
+
+	public String toString()
+	{
+		return super.toString() + ", id = " + id;
+	}
+}
